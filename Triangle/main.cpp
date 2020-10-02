bba00fcedd7b8308f7c7220b27e99ef4f2428e8f
Anastasia Bash <nastynbash@gmail.com>
2020-10-02 19:21:04 +0300

    Triangles1

---
+++ Triangle/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    scanf("%d", &n);
+
+
+    int k=n*n;
+
+    printf("%d\n", k);
+}
